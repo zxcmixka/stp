ef4d2cc37c03895667d68691d84a8aa9fed95f9f
zxcmixka <pikopavel@gmail.com>
2025-09-21 11:28:53 +0300

    server start

--- back/server.js
+++ back/server.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 import express from "express";
 
-const app = express;
+const mongoose = require('mongoose');
 
 
-async function main() {
-    
-}
+mongoose.connect('mongodb+srv://mixka:pakookap@stp.djsvi2g.mongodb.net/?retryWrites=true&w=majority&appName=stp')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+  const connect = async () => {
+  try {
+    const db = await mongoose.connect("mongodb+srv://mixka:pakookap@stp.djsvi2g.mongodb.net/?retryWrites=true&w=majority&appName=stp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+module.exports = { connect };
